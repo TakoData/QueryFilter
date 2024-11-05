465f798a38a8ba1943f661c37e5ce0420aeb61c5
noahjax <noahbjax@gmail.com>
2024-11-04 16:15:23 -0800

    pin revisions and pull spacy model

--- src/tako_query_filter/filter.py
+++ src/tako_query_filter/filter.py
@@ -1,11 +1,13 @@
 import json
 import logging
+from pathlib import Path
 import re
 import joblib
 import numpy as np
 from typing import Iterable, List, Optional, Set
 from sklearn.linear_model import LogisticRegressionCV
 from huggingface_hub import hf_hub_download
+import spacy
 from spacy.language import Language
 
 
@@ -20,50 +22,54 @@ class TakoQueryFilter:
         self.spacy_model = spacy_model
         self.keywords = keywords
         self.keyword_match_score = 0.9
+        self.embeddings_model = None
 
     @classmethod
     def load_from_hf(
         cls,
         scikit_path: str = "TakoData/ScikitModels",
-        revision: Optional[str] = None,
+        topic_revision: Optional[str] = "a8a257f706ec28a63eeb40b088b8e05b30670971",
+        spacy_revision: Optional[str] = "156303cfba1f9ac5ef7cfd35fe5dc8c9238a459d",
         force_download: bool = False,
     ):
-        chart_model = joblib.load(
-            hf_hub_download(
-                repo_id=scikit_path,
-                filename="models/chart_model.pkl",
-                revision=revision,
-                force_download=force_download,
-            )
-        )
         topic_model = joblib.load(
             hf_hub_download(
                 repo_id=scikit_path,
                 filename="models/topic_model.pkl",
-                revision=revision,
+                revision=topic_revision,
                 force_download=force_download,
             )
         )
+        spacy_model_path = hf_hub_download(
+            repo_id="TakoData/ner-model-best",
+            filename="config.cfg",
+            revision=spacy_revision,
+            force_download=force_download,
+        )
+        spacy_model_dir = str(Path(spacy_model_path).parent)
+        spacy_model = spacy.load(spacy_model_dir)
         keywords_file = hf_hub_download(
             repo_id=scikit_path,
             filename="models/keywords.json",
-            revision=revision,
+            revision=topic_revision,
             force_download=force_download,
         )
         with open(keywords_file, "r") as f:
             keywords = set(json.load(f))
 
-        return cls(chart_model, topic_model, keywords)
+        return cls(topic_model, spacy_model, keywords)
 
     def create_embeddings(self, queries: Iterable[str]) -> np.ndarray:
-        if not self.model:
+        if not self.embeddings_model:
             from sentence_transformers import SentenceTransformer
 
-            self.model = SentenceTransformer(
+            self.embeddings_model = SentenceTransformer(
                 "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
             )
 
-        embeddings = self.model.encode(list(queries), normalize_embeddings=True)
+        embeddings = self.embeddings_model.encode(
+            list(queries), normalize_embeddings=True
+        )
         return embeddings
 
     def extract_spacy_features(self, query: str) -> np.ndarray:
